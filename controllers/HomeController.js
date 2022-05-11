e27cdba364a93323d3097f2880207dc506c0344c
Mark <markyang1224@gmail.com>
2022-05-11 10:31:48 +0800

    new code

--- controllers/HomeController.js
+++ controllers/HomeController.js
@@ -2,17 +2,21 @@ const router = require("express").Router; //import express
 const axios = require("axios");
 const cheerio = require("cheerio");
 
-const index = (req, res) => {
+const index = async (req, res) => {
   async function GetDefaultData() {
     const res = await axios.get("https://rate.bot.com.tw/xrt?Lang=zh-TW"); //透過axios發http request
     const $ = cheerio.load(res.data); //將data存入$
-    const elementselector = `.table > tbody:nth-child(2) > tr`; //選擇器
+    // const elementselector = `.table > tbody:nth-child(2) > tr`; //選擇器
+    const elementselector = `tbody > tr:nth-child(1)`; //選擇器
     const keys = ["cash_buy", "cash_sell", "deposit_buy", "deposit_sell"];
     const default_data = []; //匯率總資料
 
+    //解釋: 下列的element是表示<div class="test">hi</div>這種
+
+    //抓取金額
     $(elementselector).each((parentidx, parentelem) => {
       //tr
-      console.log(parentidx);
+
       let keyidx = 0;
       let rateobj_temp = {}; //放入每行的匯率資料
 
@@ -30,12 +34,19 @@ const index = (req, res) => {
       default_data.push(rateobj_temp); //push到總資料
     });
 
-    // return default_data;
+    //抓取幣別
+    const CurrencySelector = `tbody > tr:nth-child(1) > td.currency.phone-small-font > div > div.hidden-phone.print_show`;
+    $(CurrencySelector).each((idx, elem) => {
+      let value = $(elem).text();
+      console.log(value);
+    });
+
+    return default_data;
   }
 
-  GetDefaultData();
-
-  res.send("Checking");
+  let DefaultData = await GetDefaultData();
+  console.log(DefaultData);
+  res.render("index", { DefaultData });
 };
 
 module.exports = { index };
